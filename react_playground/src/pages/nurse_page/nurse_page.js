07f348dc11b69f50b7f0c38c1470ec4a92e28a01
Manon C. Diana <manon@hygie.care>
2024-08-30 12:37:31 +0200

    add todo

--- react_playground/src/pages/nurse_page/nurse_page.js
+++ react_playground/src/pages/nurse_page/nurse_page.js
@@ -3,6 +3,8 @@ import TableComponent from '../../components/table/table_component';
 
 function Table() {
 
+    //TODO : faire ici le fetch de data et les passer en props au composant TableComponent ?
+
     return (
         <div>
             <h1>Infirmiers</h1>
